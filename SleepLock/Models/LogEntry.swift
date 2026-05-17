63e2e0fb2ab67e09628152a3e16e0a5e2dece81e
akisyo <52205626+yxxuto-choco@users.noreply.github.com>
2026-05-18 00:14:28 +0900

    Add LogEntry.swift

---
+++ SleepLock/Models/LogEntry.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+struct LogEntry: Identifiable, Codable, Hashable {
+    let id: UUID
+    let date: Date
+    let message: String
+
+    init(id: UUID = UUID(), date: Date = Date(), message: String) {
+        self.id = id
+        self.date = date
+        self.message = message
+    }
+}
